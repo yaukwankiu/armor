3168309439bd688a167bdfaa3e9d3bffd3272aa9
yaukwankiu2 <yaukwankiu@ntu.edu.tw>
2014-07-25 17:12:03 +0800

    power spec local test

--- tests/powerSpecLocalTest.py
+++ tests/powerSpecLocalTest.py
@@ -1,11 +1,15 @@
 #powerSpecLocalTest.py
+thisScript = 'powerSpecLocalTest.py'
 
+import shutil, os, time
 from armor.initialise import *
 L = monsoon.list + march.list + kongrey.list + may.list 
 ###
 outputFolder = dp.root+"labLogs2/powerSpecLocal/"
 if not os.path.exists(outputFolder):
     os.makedirs(outputFolder)
+
+shutil.copyfile(dp.root+ 'python/armor/tests/'+  thisScript, outputFolder+str(time.time())+thisScript)
 
 #plt.figure()
 
@@ -26,12 +30,16 @@ for count in range(30):
         for j in range(J):
             m1 = m.getWindow(height*i//I, width*j//J, height//I, width//J)
             try:
-                psResults[(i,j)] = m1.powerSpec(scaleSpacePower=2, outputFolder=outputFolder, 
+                #########################################################################
+                #   key line for computation
+                psResults[(i,j)] = m1.powerSpec(scaleSpacePower=0, outputFolder=outputFolder, 
                                                 bins=[0, 0.003, 0.01, 0.03, 0.1, 0.3, 1., 3., 10., 30.,100.],
                                                 sigmas  = [1, 2, 4, 8 ,16, 32, 64, 128, 256],
                                                  responseThreshold=0.00 ,   
-                                                 useOnlyPointsWithSignals=False,
+                                                 useOnlyPointsWithSignals=True,
                                                 )
+                #
+                #######################################################################
             except:
                 print "Error!\n\n==============", i, j
                 time.sleep(2)
@@ -53,6 +61,10 @@ for count in range(30):
                     plt.xlabel('log2(sigma)')
                     plt.ylabel('log10(frequency)')
                 
+                """
+                ####################################################################
+                #   the key lines for plotting
+                #
                 sigmas = psResults[(i,j)]['sigmas']
                 arr    = psResults[(i,j)]['maxSpec'].matrix
                 hist, edges = np.histogram(arr, bins=[0]+ sigmas+[999999], )
@@ -61,10 +73,32 @@ for count in range(30):
                 plt.ylim([0,8])
                 plt.plot([-1]+np.log2(sigmas).tolist(), np.log10(hist), "o-")
                 #plt.plot([-1]+np.log2(sigmas).tolist(), hist)
+                #
+                #
+                ####################################################################
+                """
+
+                ####################################################################
+                #   the key lines for plotting
+                #
+                sigmas = psResults[(i,j)]['sigmas']
+                responses    = psResults[(i,j)]['responseImages']
+                hist         = responses.sum(axis=0).sum(axis=0).tolist()    #keeping the last axis = sigmas
+                #plt.plot([0]+sigmas, np.log10(hist))
+                plt.xlim([-1,8])
+                plt.ylim([0,12])
+                plt.plot(np.log2(sigmas).tolist(), np.log10(hist), "o-")
+                #plt.plot([-1]+np.log2(sigmas).tolist(), hist)
+                #
+                #
+                ####################################################################
+
+
             except KeyError:
                 print "key error!", i, j
                 plt.imshow(m.matrix, origin='lower', cmap = m.cmap, vmin=m.vmin,vmax=m.vmax)
-                plt.colorbar()
+                #plt.colorbar()
+                plt.axis('off')
                 time.sleep(2)
     outputPath = outputFolder+ 'powerSpecLocal_' + m.dataTime + "_"+ str(int(time.time())) + '.jpg'
     print "saving to:, ", outputPath ,'\n\n###################################################'
