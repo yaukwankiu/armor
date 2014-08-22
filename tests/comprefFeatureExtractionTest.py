a99f2e28a41d1dfceb4f81c03c826315e51d7769
yan <yan@yan-Aspire-E1-571G.(none)>
2014-08-22 12:39:22 +0800

    compref feature extraction and classificaiton test - fix

--- tests/comprefFeatureExtractionTest.py
+++ tests/comprefFeatureExtractionTest.py
@@ -5,17 +5,17 @@ import os
 from matplotlib import pyplot as plt
 from armor import objects4 as ob
 from armor import defaultParameters as dp
-outputFolder = dp.root+ 'labLogs2/featureExtraction/'
+outputFolder = dp.root + 'labLogs2/featureExtraction/'
 if not os.path.exists:
     os.makedirs(outputFolder)
-testComprefs = ob.may2014.list[20:60:6] + ob.monsoon[20:40:6] + ob.march2014.list[20:40:6] +\
-               ob.kongrey.list[20:40:6]
+testComprefs = ob.may2014.list[20:60:6] + ob.monsoon[20:40:6] + \
+               ob.march2014.list[20:40:6] + ob.kongrey.list[20:40:6]
 
 classificationResults = []
-for a in testComprefs: 
+for a in testComprefs:
     t0 = time.time()
     plt.close()
-    print '\n------------------------------------------\n',a.name
+    print '\n------------------------------------------\n', a.name
     a.load()
     a.initialiseFeatures()
     print 'granulometry features'
@@ -24,7 +24,8 @@ for a in testComprefs:
     a.gaborFeatures(multiplier=10.)
     result = a.classify()
     classificationResults.append(result)
-    pickle.dump(res, open(outputFolder+ 'feature_classification' + str(time.time())+ a.name + '.pydump' ))
+    pickle.dump(result, open(outputFolder + 'feature_classification' +\
+                 str(time.time()) + a.name + '.pydump'))
     t1 = time.time()
-    print 'time spent for', a.name , t1-t0, 'seconds'
+    print 'time spent for', a.name, t1 - t0, 'seconds'
     print '\n-------------------------------------------\n'
