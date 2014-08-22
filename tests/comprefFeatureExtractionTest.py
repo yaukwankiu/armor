87f0c8fe0fecad811afb0d0282368b93d8c27c04
yan <yan@yan-Aspire-E1-571G.(none)>
2014-08-22 13:58:11 +0800

    fix

--- tests/comprefFeatureExtractionTest.py
+++ tests/comprefFeatureExtractionTest.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from armor import objects4 as ob
 from armor import defaultParameters as dp
 outputFolder = dp.root + 'labLogs2/featureExtraction/'
-if not os.path.exists:
+if not os.path.exists(outputFolder):
     os.makedirs(outputFolder)
 testComprefs = ob.may2014.list[20:60:6] + ob.monsoon[20:40:6] + \
                ob.march2014.list[20:40:6] + ob.kongrey.list[20:40:6]
@@ -25,7 +25,7 @@ for a in testComprefs:
     result = a.classify()
     classificationResults.append(result)
     pickle.dump(result, open(outputFolder + 'feature_classification' +\
-                 str(time.time()) + a.name + '.pydump'))
+                 str(time.time()) + a.name + '.pydump', 'w'))
     t1 = time.time()
     print 'time spent for', a.name, t1 - t0, 'seconds'
     print '\n-------------------------------------------\n'
