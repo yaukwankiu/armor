57e73e339aec51d27e79d78e970fcce3ed9cc485
yan <yan@yan-Aspire-E1-571G.(none)>
2014-09-14 06:10:00 +0800

    adding a loadImage function for charts2-type images from CWB

--- basicio/loadImage.py
+++ basicio/loadImage.py
@@ -1,15 +1,19 @@
 #loadImage.py
 #    to load image to matrix
-
+import os
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from armor import defaultParameters as dp
 
 
-def loadImage(a, dataTime="", dataPath="", imageType="hs1p", imageSuffix='.jpg', verbose=True, 
+def loadImage(a, dataTime="", dataPath="", 
+                #imageType="hs1p", 
+                imageType='charts2',
+                imageSuffix='.jpg', verbose=True, 
                multiplier=50.,
                rawImage=False,
+               medianFilterSize=20, #for removing unwanted lines in charts2
                *args, **kwargs):
 
     if dataPath =="":
@@ -20,7 +24,14 @@ def loadImage(a, dataTime="", dataPath="", imageType="hs1p", imageSuffix='.jpg',
         elif a.dataTime!='':
             dataTime = a.dataTime
             dataTimeString = a.dataTime[0:4] + '-' + a.dataTime[4:6] + '-' + a.dataTime[6:8] + "_" + a.dataTime[9:13]
-            dataPath = dp.root + '../CWB/' + imageType + "/"+ dataTimeString[:10] + '/' + dataTimeString + imageSuffix
+            dataFolder = dp.root + '../CWB/' + imageType + "/"+ dataTimeString[:10] + '/'
+            try:
+                dataFolderList = os.listdir(dataFolder)
+                dataFileName = [v for v in dataFolderList if dataTimeString in v and imageSuffix in v][0]
+            except IndexError:
+                dataFileName = dataFolderList[0]
+            dataPath = dataFolder + dataFileName
+            print "dataFileName:", dataFileName
         elif a.dataPath!="":
             dataPath = a.dataPath
         else:
@@ -40,6 +51,28 @@ def loadImage(a, dataTime="", dataPath="", imageType="hs1p", imageSuffix='.jpg',
             #img = np.flipud(img)
             img2= ((img[:,:,0] > threshold) * (img[:,:,1] > threshold) * (img[:,:,2]>threshold)).astype(int)
             a.matrix = np.ma.array(img2, fill_value=-999)
+            a.matrix*=multiplier
+            a.matrix.mask=False   
+            a.matrix.set_fill_value(-999)
+            a.setMaxMin()
+            return a
+        elif imageType == 'charts2' and rawImage==False:
+            from scipy import ndimage
+            l   = plt.imread(dataPath)
+            # adapted from armor/tests/imageToData_chartsTest.py
+            l5= ndimage.filters.median_filter(l[:,:,0], size=medianFilterSize)
+            l6= ndimage.filters.median_filter(l[:,:,1], size=medianFilterSize)
+            l7= ndimage.filters.median_filter(l[:,:,2], size=medianFilterSize)
+            l8  = l.copy()
+            l8[:,:,0] = l5
+            l8[:,:,1] = l6
+            l8[:,:,2] = l7
+            l81 = (l5>160) *( l6>160) *( l7>160)
+            l81[550:, :210] = 1
+            l81[:250, :50] = 1
+            l81 = 1- l81
+            img = l81
+            a.matrix = np.ma.array(img, fill_value=-999)
             a.matrix*=multiplier
             a.matrix.mask=False   
             a.matrix.set_fill_value(-999)
