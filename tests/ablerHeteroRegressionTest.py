66d3fe622cb9b75d79c22d816f35d186b2f9edf8
yan <yan@yan-Aspire-E1-571G.(none)>
2014-10-15 17:39:09 +0800

    abler index test

--- tests/ablerHeteroRegressionTest.py
+++ tests/ablerHeteroRegressionTest.py
@@ -56,12 +56,38 @@ time.sleep(2)
 #x=a.shiiba(b)
 
 ###################################################################################
-intensityFactor = 1.00     #   <-- edit here
-axis1factor     = 0.995
-axis2factor     = 1.005
-testGoal = 'Axes Transformation, Translation and Rotation and intensity adjustment by '+str(intensityFactor)
+#intensityFactor = 1.00     #   <-- edit here
+#axis1factor     = 0.995
+#axis2factor     = 1.005
 a=pattern.a.load()
 a2 = a.getRectangle(400,400,200,200)
+b=pattern.b.load()
+b2 = b.getRectangle(400,400,200,200)
+b3 = b.getRectangle(500,600,200,200)
+c  = dbz('20120612.0800').load()
+c2 = c.getRectangle(300,400,200,200)
+c3 = c.getRectangle(200,200,200,200)
+c4 = c.getRectangle(400,600,200,200)
+d  = dbz('20120612.0900').load()
+d2 = d.getRectangle(400,400,200,200)
+d3 = d.getRectangle(500,500,200,200)
+
+dbzs = [d2, d3, c4, b2,a2, b3, c2, c3, ]
+for a in dbzs:
+    a.backupMatrix(0)
+angles =[0.1, 0.2, 0.5, 1.,]
+#axis1factors = [0.8, 0.9, 0.95, 0.99, 0.995, 1., 1.005, 1.01, 1.05, 1.1, 1.2]
+#axis2factors = [0.8, 0.9, 0.95, 0.99, 0.995, 1., 1.005, 1.01, 1.05, 1.1, 1.2]
+#intensityFactors = [0.9, 0.95, 0.99, 0.995, 1., 1.005, 1.01, 1.05, 1.1]
+axis1factors = [0.9, 0.95, 0.99, 0.995, 1.,  1.01,]
+axis2factors = [0.8, 0.9, 0.95, 0.99, 0.995, 1., 1.1, ]
+intensityFactors = [0.95, 1., 1.01,]
+
+import itertools
+testCases = itertools.product(dbzs, angles, axis1factors, axis2factors, intensityFactors)
+
+
+testGoal = 'Axes Transformation, Translation and Rotation and intensity adjustment by intensity '
 #a2.coordinateOrigin= (100,100)
 print '\n========================\n'
 a2.backupMatrix(0)
@@ -69,7 +95,11 @@ outputFolder= dp.root+'labLogs2/ABLERselectionIndex/'
 outputString = "\n====================================\n"
 outputString += time.asctime() +'\n'
 outputString += "Test Goal: " + testGoal
-outputString +="\n\nangle" 
+outputString += "\na2.name"
+outputString += "\tangle"
+outputString += "\taxis1factor"
+outputString += "\taxis2factor"
+outputString += "\tintensityFactor"
 outputString +="\tR-squared" 
 outputString +="\tdisplacement" 
 outputString +="\tT" 
@@ -78,7 +108,8 @@ outputString += "\tC"
 outputString +='\n'
 open(outputFolder+'log.txt', 'a').write(outputString)
 
-for angle in [0.1, 0.2, 0.5, 1.,]:
+testCases = itertools.product(dbzs, angles, axis1factors, axis2factors, intensityFactors)
+for a2, angle, axis1factor, axis2factor, intensityFactor in testCases:
     T = np.array([[axis1factor,0.],[0.,axis2factor]])
     T = tr.rotationMatrix(rad=-np.pi*angle/360)*T
     displacement = np.array([[1],[4]])
@@ -103,12 +134,19 @@ for angle in [0.1, 0.2, 0.5, 1.,]:
     vect.saveImage()
 
     pickle.dump(x2, open(outputFolder+'%d_x.pydump' % int(time.time()),'w'))
-    outputString = str(angle)
-    outputString += "\t"+str(x2['Rsquared'])
-    outputString += "\t"+str(displacement.T)
-    outputString +=  "\t"+str(T.round(3).flatten())
-    outputString +=  "\t"+str(x2['mn'])
-    outputString +=  "\t" + str(x2['C'].round(3).flatten())
+
+    outputString = str(a2.name)
+    outputString += '\t,' + str(angle)
+    outputString += "\t," + str(axis1factor)
+    outputString += "\t," + str(axis2factor)
+    outputString += "\t," + str(intensityFactor)
+
+    outputString += "\t,"+str(x2['Rsquared'])
+    outputString += "\t,"+str(displacement.flatten())
+    outputString +=  "\t," + ' '.join([str(v) for v in np.array(T).reshape(T.size)])
+    outputString +=  "\t,"+str(x2['mn'])
+    #outputString +=  "\t," + str(x2['C'].round(3).flatten()) #doesn't work
+    outputString += "\t," + ' '.join([str(v) for v in np.array(x2['C'].round(3))])
     outputString += '\n'
     #open(outputFolder+'log.txt','a').write(outputString)
     # for excel
