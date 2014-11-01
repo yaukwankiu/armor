39e6e53caf109d7565bdcf8c3ffc7f7750c0d987
yan <yan@yan-Aspire-E1-571G.(none)>
2014-11-01 14:36:24 +0800

    distribution test

--- tests/localFeaturesDistributionTest.py
+++ tests/localFeaturesDistributionTest.py
@@ -13,11 +13,14 @@ dp  = pattern.dp
 plt = pattern.plt
 np  = pattern.np
 from armor import objects4 as ob
-#
-timeStamp0   = str(int(time.time()))
 outputFolder = dp.root + 'labLogs2/charts2_local_features_distribution/'
 if not os.path.exists(outputFolder):
     os.makedirs(outputFolder)
+
+##############################################################################
+#   collecting data
+#
+timeStamp0   = str(int(time.time()))
 #
 comprefs    = sum([v.list for v in ob.comprefs], [])
 wrfs        = sum([v.list for v in ob.wrfs], [])
@@ -39,4 +42,93 @@ for imageList, threshold, toAverage in testCases:
         a.localShapeFeatures()
         filePath  = outputFolder + str(int(time.time())) + "_" + a.name + '.pydump'
         pickle.dump(a.localFeatures, open(filePath,'w'))
-        
+#
+#   end collecting data
+#
+##############################################################################
+
+##############################################################################
+#   summarising
+#
+
+#def summarising(folder=outputFolder):
+folder  = outputFolder
+chartsFolder = folder + 'charts/'
+if not os.path.exists(chartsFolder):
+    os.makedirs(chartsFolder)
+L   = os.listdir(folder)
+L   = [v for v in L if '.pydump' in v]
+fileName = L[0]
+x   = pickle.load(open(folder+fileName))
+
+lf  = x['localFeatures'][0]
+keys = lf.keys()
+
+def keySize(dic, k):
+    try:
+        n = dic[k].size
+    except AttributeError:
+        try:
+            n = dic[k].__len__()
+        except AttributeError:
+            n = 1
+    return n 
+
+keySizes = [keySize(lf, v) for v in lf]   
+print keySizes
+print [(keys[v], keySizes[v]) for v in range(len(keys))]
+keysFull =  sum([[keys[i]]*keySizes[i] for i in range(len(keys))],[])
+print keysFull
+
+#for lf in x['localFeatures']:
+feats = []
+for k in keys:
+    print k,
+    try:
+        feats.extend(lf[k].flatten())
+    except AttributeError:
+        try:
+            feats.extend(lf[k])
+        except TypeError:
+            feats.append(lf[k])
+#print feats
+
+featsArr = np.zeros((1, len(keysFull)))
+for fileName in L:
+    x   = pickle.load(open(folder+fileName))
+    for lf in x['localFeatures']:
+        feats = []
+        for k in keys:
+            #print k,
+            try:
+                feats.extend(lf[k].flatten())
+            except AttributeError:
+                try:
+                    feats.extend(lf[k])
+                except TypeError:
+                    feats.append(lf[k])
+        featsArr = np.vstack([featsArr, feats])
+        print feats[:4]
+
+featsFile = open(folder+str(int(time.time()))+'featuresArr.dat','a')
+featsFile.write('#' + '  '.join(keysFull) + '\n')
+np.savetxt(featsFile, featsArr)
+featsFile.close()
+
+###############################################################################
+#   plotting
+#
+folder = outputFolder
+fileName = '1414755088featuresArr.dat'
+featsFile = open(folder+fileName)
+featsArr = np.loadtxt(featsFile)
+
+for i in range(len(keysFull)):
+    feat0 = featsArr[:,i]
+    y, x = np.histogram(feat0, 50)
+    plt.clf()
+    plt.plot(x[:-1], y)
+    plt.title(keysFull[i]+str(i))
+    plt.savefig(chartsFolder+str(int(time.time()))+keysFull[i]+str(i))
+    plt.show(block=False)
+    
